0a2cb7465a369020037fb57548a6eb7ef34ceea4
long phan <longphan@longs-MacBook-Pro.local>
2015-11-25 04:48:13 -0500

    added checkin date box

--- js/travel.js
+++ js/travel.js
@@ -8,6 +8,22 @@ $(document).ready(function(){
   
 
 
+//date picker
+$(function() {
+    var today = new Date();
+    
+    $('#from-datepicker').datetimepicker({
+      language: 'pt-BR',
+      startDate: today
+    });
+    
+     $('#to-datepicker').datetimepicker({
+      language: 'pt-BR'
+    });
+    
+});
+
+
 
 
 
