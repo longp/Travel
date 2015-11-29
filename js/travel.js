d38d8f72764aef515ab2dbd8ac85b268c386972b
long phan <longphan@longs-MacBook-Pro.local>
2015-11-29 02:45:21 -0500

    fixed issue with search panel

--- js/travel.js
+++ js/travel.js
@@ -4,7 +4,6 @@ $(document).ready(function(){
   $('.carousel').carousel({
 
   interval: 3000
-<<<<<<< HEAD
 })
 
 
@@ -28,35 +27,10 @@ $("#searchbtn").on("click", function(){
 
 
 
-=======
-  })
 
-  //date picker
-  // $(function() {
-  //   var today = new Date();
-    
-  //   $('#from-datepicker').datetimepicker({
-  //     language: 'pt-BR',
-  //     startDate: today
-  //   });
-    
-  //    $('#to-datepicker').datetimepicker({
-  //     language: 'pt-BR'
-  //   });     
-  // });
-
-  // $("input").on('click', function() {
-  //   alert("ye");
-    // var text = $(this).html();
-    // $(".dropdown-menu ul li").html(text);
-    // $(".dropdown-menu").hide();
-  // });
-<<<<<<< HEAD
->>>>>>> 895280fb0d97f248e785017bc31ce70fcd149bbf
-=======
   $(".modal-footer .btn-primary").on("click", function(){
     $(this).parents("div.listing").hide();
     $('#myModal').modal('hide')
   });
->>>>>>> master
+
 });
