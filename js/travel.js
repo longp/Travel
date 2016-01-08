c270fbb49fbaff28365eb83c526382648f0d014e
eliezerwohl <eliezerwohl@gmail.com>
2016-01-08 17:37:14 -0500

    put page in container, fixed bug where adding amenities messed up panel

--- js/travel.js
+++ js/travel.js
@@ -1,42 +1,35 @@
 $(document).ready(function(){
-
   $(".btn-group2").hide();
   $(".listing").hide();
   $(".alert").hide();
-
   //enable popovers
   $(function () {
-    $('[data-toggle="popover"]').popover()
+    $('[data-toggle="popover"]').popover();
   });
-  
   //carousel interval
   $('.carousel').carousel({
     interval: 3000
   });
-
 //show search results 
-  
-$("#btnsearch").on("click", function(e){
-  e.preventDefault();
-  amenResult();
-  var city = $("#city").val();
-  if (city === ""){
-    $(".alert").show();
-    return;
-  } else if (city === "London"){
-    $(".listing.ld").show();
-  }else if (city === "Paris"){
-    $(".listing.pa").show();
-  }else if (city === "Tokyo"){
-    $(".listing.tk").show();
-  }else if (city === "New York City"){
-    $(".listing.ny").show();
-  }else if (city === "Hong Kong"){
-    $(".listing.hk").show();
-  }
-
-});
-
+  $("#btnsearch").on("click", function(e){
+    e.preventDefault();
+    amenResult();
+    var city = $("#city").val();
+    if (city === ""){
+      $(".alert").show();
+      return;
+    } else if (city === "London"){
+      $(".listing.ld").show();
+    }else if (city === "Paris"){
+      $(".listing.pa").show();
+    }else if (city === "Tokyo"){
+      $(".listing.tk").show();
+    }else if (city === "New York City"){
+      $(".listing.ny").show();
+    }else if (city === "Hong Kong"){
+      $(".listing.hk").show();
+    }
+  });
   //amenity result
   function amenResult () {
     $("#op1").one("click", function(){
@@ -64,24 +57,18 @@ $("#btnsearch").on("click", function(e){
       $(".amenities").append(spa + ", ");
     });
   };
-
-
   $('.selectpicker').selectpicker();
-
   $('.selectpicker').selectpicker({
       style: 'btn-info',
       size: 4
   });
-
   // debugger;
   $(document).on("click",".modal-footer .btn-primary", function(){
     $(this).parents("div.listing").hide();
     $('.modal').modal('hide')
   });
-
   //show advanced search option buttons
   $("#moreOptions").on("click", function(){
     $(".btn-group2").fadeToggle();
   });
-
 });
